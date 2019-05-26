e4eb26045b5c488e9e04baacaa3e63bd09d52b43
Diotr <35659983+Diotr@users.noreply.github.com>
2019-05-26 21:18:49 +0100

    moose button doesn't work

--- website.js
+++ website.js
@@ -1,6 +1,15 @@
 var someText = "imie i nazwiskło";
+
+
 function dodajText(){
 //place someText variable on website
 document.getElementById("myText").innerHTML = someText;
 console.log("Działa");
+}
+function addImg(){
+    var img1 =document.createElement("img");
+img1.src = "C:\Users\pmich\OneDrive\Documents\website.js\moose.jpg";
+   document.getElementById("moo").appendChild(img1);
+  console.log("moose");
+
 }
